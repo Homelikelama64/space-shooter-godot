cc31c8030318d5e9b59de7b5b695ca20b745be9f
Homelikelama64 <homelikelama64@gmail.com>
2024-12-30 23:02:54 +1300

    Rebalanced the game

--- classes/part_class.gd
+++ classes/part_class.gd
@@ -5,10 +5,13 @@ class_name Part
 
 @export var health:float
 @export var starting_health:float
+@export var regen:float
 @export var size:float
 @export var Name:String
 
-func _process(delta: float) -> void:
+func _process(dt: float) -> void:
+	if !Engine.is_editor_hint():
+		health = clamp(health + regen * dt,0,starting_health)
 	queue_redraw()
 
 func _draw() -> void:
